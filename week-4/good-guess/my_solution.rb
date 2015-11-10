d83b75320363af626fb876d9426079ba97ca288e
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2015-11-10 15:11:33 -0500

    Good guess

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,14 @@
+# Good Guess
+
+# I worked on this challenge with Mo.
+
+
+# Your Solution Below
+
+def good_guess?(integer)
+  if integer == 42
+    return true
+  else
+    return false
+  end
+end
