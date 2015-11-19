b579de89bae90851ee68de63c07e2050ba59f513
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2015-11-19 17:44:32 -0500

    Solo challenge

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,6 +1,6 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [] hours on this challenge.
+# I spent 4 hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -18,95 +18,46 @@
 # 3. string is > 3 digits, and length % 3 == 1 (so there will be 1 digit on the left of the left-most comma)
 # 4. string is > 3 digits, and length % 3 == 2 (so there will be 2 digits on the left of the left-most comma)
 # For cases 2-4, I will do the following:
-#   loop through the 3 digits on the right, push them to an array, then put them back into a string of length 3 (there is probably an easier way to do this, I will investigate when refractoring)
-#   repeat this process to go through the next 3 digits in the string, and push them to the same array, adding a comma in between
+# grab the 3 left-most elements (after the first comma) and add them to a string, adding a comma. repeat this process until all groups of 3 are grabbed.
+# last step will be to modify depending if the digit length is in case 2, 3, 4
 
 # 1. Initial Solution
 
 def separate_comma(integer)
   string = integer.to_s
+  new_string = ""
 
-  new_array = []
-
-  if string.length < 4
-    return string
-
-  elsif string.length % 3 == 0
-
-    j = string.length/3
-    while j > 0
-
-      new_string = ""
-
-      for i in (-3*(j)+0)..(-3*(j)+2)
-        new_array << string[i]
-      end
-
-      new_array << ","
-
-      for i in 0..new_array.length-1
-        new_string << new_array[i]
-      end
-
+  j = string.length/3
+  while j > 0
+    new_string << string[((-3)*j),3] << ","
     j -= 1
-    end
-
-  elsif string.length % 3 == 1
-
-    new_array << string[0]
-    new_array << ","
-
-    j = string.length/3
-    while j > 0
-
-      new_string = ""
-
-      for i in (-3*(j)+0)..(-3*(j)+2)
-        new_array << string[i]
-      end
-
-      new_array << ","
-
-      for i in 0..new_array.length-1
-        new_string << new_array[i]
-      end
-
-    j -= 1
-    end
-
-  elsif string.length % 3 == 2
-
-    new_array << string[0]
-    new_array << string[1]
-    new_array << ","
-
-    j = string.length/3
-    while j > 0
-
-      new_string = ""
-
-      for i in (-3*(j)+0)..(-3*(j)+2)
-        new_array << string[i]
-      end
-
-      new_array << ","
-
-      for i in 0..new_array.length-1
-        new_string << new_array[i]
-      end
-
-    j -= 1
-    end
-
   end
 
-  return new_string.chomp(",")
+  if string.length % 3 == 0
+    return new_string.chomp(",")
+  elsif string.length % 3 == 1
+    return "#{string[0]}," << new_string.chomp(",")
+  elsif string.length % 3 == 2
+    return "#{string[0,2]}," << new_string.chomp(",")
+  end
 
 end
 
 # 2. Refactored Solution
 
+def separate_comma(integer)
 
+  new_string = integer.to_s.reverse.scan(/.../).join(',').reverse
+
+  if integer.to_s.length % 3 == 0
+    return new_string
+  elsif integer.to_s.length % 3 == 1
+    return "#{integer.to_s[0]}," << new_string
+  elsif integer.to_s.length % 3 == 2
+    return "#{integer.to_s[0,2]}," << new_string
+  end
+
+end
 
 
 # 3. Reflection
@@ -116,6 +67,135 @@ end
 # Was your pseudocode effective in helping you build a successful initial solution?
 #   I found this challenge difficult and my initial pseudocode had to be modified a lot once I actually started coding. I think this is typical however, especially for a beginner. However, the initial idea I had was what I needed to implement, I just had trouble actually figuring out the best way using Ruby.
 # What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+#   The Ruby methods I used were really helpful - I used scan and join as a quicker way to grab digits in the integer 3 at a time and then join them with a comma. I also used the reverse method to get around the fact that we are adding commas starting from the right but all our array indexing starts at the left.
 # How did you initially iterate through the data structure?
-#   In my initial solution, I created several "if" statements to separate the strings where length <=3, length % 3 == 0, length % 3 == 1, and length % 3 == 2. Then I iterated through each string starting on the right to add groups of 3 integers into an array. I saved that array and added a comma and repeated this process to get through the entire string, with different handling of the first few integers in the string (to the left of the first comma) depending on the "if" statement group we were in.
+#   In my initial solution, I iterated through the data structure by grouping the integer 3 digits at a time and adding that to a longer string separated by commas. Then depending on the length of the integer and what bucket it fell in, I added the digits to the left of the first comma manually, if necessary.
 # Do you feel your refactored solution is more readable than your initial solution? Why?
+#   Definitely - it's much shorter and more readable. I also had older versions that are below that show my initial thought process, which was much more drawn out. I think using scan and join in my refactored solution is much cleaner and also doesn't require manually removing a comma at the end.
+
+
+
+# OLD SOLUTIONS (not for peer review)
+
+# Solution 1
+# def separate_comma(integer)
+#   string = integer.to_s
+
+#   new_array = []
+
+#   if string.length < 4
+#     return string
+
+#   elsif string.length % 3 == 0
+
+#     j = string.length/3
+#     while j > 0
+
+#       new_string = ""
+
+#       for i in (-3*(j)+0)..(-3*(j)+2)
+#         new_array << string[i]
+#       end
+
+#       new_array << ","
+
+#       for i in 0..new_array.length-1
+#         new_string << new_array[i]
+#       end
+
+#     j -= 1
+#     end
+
+#   elsif string.length % 3 == 1
+
+#     new_array << string[0]
+#     new_array << ","
+
+#     j = string.length/3
+#     while j > 0
+
+#       new_string = ""
+
+#       for i in (-3*(j)+0)..(-3*(j)+2)
+#         new_array << string[i]
+#       end
+
+#       new_array << ","
+
+#       for i in 0..new_array.length-1
+#         new_string << new_array[i]
+#       end
+
+#     j -= 1
+#     end
+
+#   elsif string.length % 3 == 2
+
+#     new_array << string[0]
+#     new_array << string[1]
+#     new_array << ","
+
+#     j = string.length/3
+#     while j > 0
+
+#       new_string = ""
+
+#       for i in (-3*(j)+0)..(-3*(j)+2)
+#         new_array << string[i]
+#       end
+
+#       new_array << ","
+
+#       for i in 0..new_array.length-1
+#         new_string << new_array[i]
+#       end
+
+#     j -= 1
+#     end
+
+#   end
+
+#   return new_string.chomp(",")
+
+# end
+
+
+# Solution 2
+# def separate_comma(integer)
+#   string = integer.to_s
+
+#   if string.length < 4
+#     return string
+
+#   elsif string.length % 3 == 0
+
+#     new_string = ""
+#     j = string.length/3
+#     while j > 0
+#       new_string << string[((-3)*j),3] << ","
+#       j -= 1
+#     end
+
+#   elsif string.length % 3 == 1
+
+#     new_string = "#{string[0]},"
+#     j = string.length/3
+#     while j > 0
+#       new_string << string[((-3)*j),3] << ","
+#       j -= 1
+#     end
+
+#   elsif string.length % 3 == 2
+
+#     new_string = "#{string[0,2]},"
+#     j = string.length/3
+#     while j > 0
+#       new_string << string[((-3)*j),3] << ","
+#       j -= 1
+#     end
+
+#   end
+
+#   return new_string.chomp(",")
+
+# end
