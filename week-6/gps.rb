cc55a6607f9c5f1b5f2a354903af0bb53ec51c27
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2015-11-23 19:06:14 -0500

    GPS 2.3 intermediary

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -41,3 +41,43 @@ p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
 
+
+
+def serving_size_calc(item_to_make, order_quantity)
+  dessert_items = {"cookie" => 1, "cake" =>  5, "pie" => 7, "chocolate" => 2, "tart" => 4}
+
+  if dessert_items[item_to_make] == nil
+    raise ArgumentError.new("#{item_to_make} is not a valid input")
+  end
+
+  servings = order_quantity/dessert_items[item_to_make]
+
+  leftover_quantity = order_quantity % dessert_items[item_to_make]
+  alternate_items = []
+
+  if leftover_quantity == 0
+    return "Calculations complete: Make #{servings} of #{item_to_make}"
+  else
+
+    dessert_items.each do |key, value|
+
+      if value <= leftover_quantity
+        alternate_items << key
+      end
+    end
+
+    return "Calculations complete: Make #{servings} of #{item_to_make}, you have #{leftover_quantity} leftover ingredients. Suggested baking items: #{alternate_items.join(", ")} "
+
+  end
+
+end
+
+p serving_size_calc("pie", 7)
+p serving_size_calc("tart", 8)
+p serving_size_calc("chocolate", 6)
+p serving_size_calc("cake", 7)
+p serving_size_calc("cookie", 1)
+p serving_size_calc("cookie", 10)
+# p serving_size_calc("THIS IS AN ERROR", 5)
+
+
