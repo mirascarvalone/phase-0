c0f429888e7dd7cbf10eec8a33f444d4c9d57064
Mira Scarvalone <mirascarvalone@Miras-MBP.home>
2015-11-27 15:05:12 -0500

    6.4 Final

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -6,9 +6,9 @@
 
 # Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: name, greeting text
+# Output: two methods, one which takes a name and stores it, the other which references that name and prings a greeting
+# Steps: create namedata method that takes name, create greetings method that prints out name/greeting
 
 class NameData
 
@@ -49,23 +49,27 @@ mira.name
 # Release 2:
 # What changed between the last release and this release?
 #   In this release, we used attr_reader to create the age variable, instead of having to define a method what_is_age later in the code.
-# # What was replaced?
+# What was replaced?
 #   The what_is_age method is replaced by attr_reader :age
-# # Is this code simpler than the last?
+# Is this code simpler than the last?
 #   It is simpler and more readable. I imagine it will be extended to the other instance variables.
 
 # Release 3:
 # What changed between the last release and this release?
-  # In this release we also use the attr_writer to create the age variable. This allows us to modify the age variable without creating a separate method, change_my_age.
+#   In this release we also use the attr_writer to create the age variable. This allows us to modify the age variable without creating a separate method, change_my_age.
 # What was replaced?
-  # The change_my_age method was replaced with attr_writer :age.
+#   The change_my_age method was replaced with attr_writer :age.
 # Is this code simpler than the last?
-  # It is simpler and more readable.
+#   It is simpler and more readable.
 
 # Release 5:
 # What is a reader method?
-  # A reader method
+#   A reader method stores a variable and allows us to access it outside of a class.
 # What is a writer method?
+#   A writer method stores a variable and allows us to access and modify it outside of a class.
 # What do the attr methods do for you?
+#   The attr methods create a reader and writer method in one step.
 # Should you always use an accessor to cover your bases? Why or why not?
+#   Not necessarily, since we may not always want to be able to modify certain variables at later parts of our code.
 # What is confusing to you about these methods?
+#   I intitially found these methods confusing but these exercises helped clear some things up.
