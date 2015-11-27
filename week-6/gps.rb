d6b82efed0706af794caec8b1705076e3be6994b
Mira Scarvalone <mirascarvalone@Miras-MBP.home>
2015-11-27 16:06:18 -0500

    2.3 GPS final

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,43 +1,50 @@
 # Your Names
-# 1)
-# 2)
+# 1) Mira
+# 2) Jenna
 
-# We spent [#] hours on this challenge.
+# We spent 1 hour on this challenge.
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  dessert_items = {"cookie" => 1, "cake" =>  5, "pie" => 7}
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
-
-  if error_counter > 0
+  if dessert_items[item_to_make] == nil
     raise ArgumentError.new("#{item_to_make} is not a valid input")
   end
 
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
+  servings = order_quantity/dessert_items[item_to_make]
 
-  case serving_size_mod
-  when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+  leftover_quantity = order_quantity % dessert_items[item_to_make]
+  alternate_items = []
+
+  if leftover_quantity == 0
+    return "Calculations complete: Make #{servings} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    dessert_items.each do |key, value|
+      if value <= leftover_quantity
+        alternate_items << key
+      end
+    end
+
+  return "Calculations complete: Make #{servings} of #{item_to_make}, you have #{leftover_quantity} leftover ingredients. Suggested baking items: #{alternate_items.join(", ")} "
+
   end
+
 end
 
-p serving_size_calc("pie", 7)
-p serving_size_calc("pie", 8)
-p serving_size_calc("cake", 5)
+p serving_size_calc("pie", 12)
 p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
-p serving_size_calc("THIS IS AN ERROR", 5)
+# p serving_size_calc("THIS IS AN ERROR", 5)
 
-#  Reflection
 
+# What did you learn about making code readable by working on this challenge?
+#   It was an interesting experience to take someone else's code and make it better. There were several ways our original code was not written in the most intuitive way, and that made it difficult to figure out what the code was doing in the first place.
+# Did you learn any new methods? What did you learn about them?
+#   I learned how to print multiple items in a text field. Previously I had only printed out one item by typing #{}, but here we made an array of possible items to make with leftover ingredients.
+# What did you learn about accessing data in hashes?
+#   I learned that when an item that isn't one of the keys in a hash (like "THIS IS AN ERROR") is passed into a hash, the output will be nil.
+# What concepts were solidified when working through this challenge?
+#   It was good practice to work on iterating through an array and adding keys or values from a hash into a new array.
